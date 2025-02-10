ff483f9831443c8765bb74f8d2cbde65e387a7a2
pascal1888 <36227438+pascalvenema@users.noreply.github.com>
2025-02-10 17:20:52 +0100

    Fixed SNOMED typo

--- src/femr/pat_utils.py
+++ src/femr/pat_utils.py
@@ -7,7 +7,7 @@ def get_patient_birthdate(patient: meds.Patient) -> datetime.datetime:
     for e in patient["events"]:
         for m in e["measurements"]:
             if (
-                m["code"] == meds.birth_code or m["code"] == "SNOMED/184099003"
-            ):  # Added SNOMED/184099003 for compatibility with EHRSHOT dataset
+                m["code"] == meds.birth_code or m["code"] == "SNOMED/3950001"
+            ):  # Added SNOMED/3950001 for compatibility with EHRSHOT dataset
                 return e["time"]
     raise ValueError("Couldn't find patient birthdate -- Patient has no events " + str(patient["events"][:5]))
